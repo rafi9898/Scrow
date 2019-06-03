dfd428052b1a0ffb5a2d9d9560d841046c0a9bb5
rafi9898 <r-podraza@wp.pl>
2019-06-03 21:06:21 +0200

    Updated styles Links in Navbar

--- scrow-app/src/components/Navbar/styled/NavbarStyled.tsx
+++ scrow-app/src/components/Navbar/styled/NavbarStyled.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Link } from "react-router-dom";
 
 export const StyledNavbar = styled.nav`
@@ -26,10 +26,28 @@ export const StyledListElement = styled.li`
   margin-right: 20px;
 `;
 
+const slideBorder = keyframes`
+0% {
+  border-width: 0%;
+  border-bottom: 1px solid #ffd02f;
+}
+
+100% {
+  border-width: 100%;
+  border-bottom: 1px solid #ffd02f;
+}
+`;
+
 export const StyledHref = styled(Link)`
   font-size: 0.9rem;
   color: #636363;
   font-weight: 500;
   text-transform: uppercase;
   text-decoration: none;
+  transition: all 0.3s;
+
+  &:hover {
+    transition: all 0.3s;
+    animation: ${slideBorder} 0.3s 0s both;
+  }
 `;
